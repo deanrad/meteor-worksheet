60e6d228c094c8327f15c3980a94bd669f33adc5
Dean Radcliffe <dradcliffe@opinionlab.com>
2014-12-04 15:00:05 -0600

    Proper exports

--- package.js
+++ package.js
@@ -1,12 +1,13 @@
 Package.describe({
   summary: 'Turn JS objects into reactive Spreadsheet-like objects, hence the name "worksheet"',
-  version: '0.0.1',
+  version: '0.0.2',
   git: 'https://github.com/chicagogrooves/deanius-worksheet'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('METEOR@1.0');
   api.addFiles('deanius:worksheet.js', 'client');
+  api.export('worksheet', ['client']);
 });
 
 Package.onTest(function(api) {
