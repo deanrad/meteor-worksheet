a98905ae0bc95642c4d80a974d391f1c1a34e78e
Dean Radcliffe <dradcliffe@opinionlab.com>
2015-03-31 16:23:06 -0500

    tweak

--- package.js
+++ package.js
@@ -1,18 +1,18 @@
 Package.describe({
-  summary: 'Turn JS objects into reactive Spreadsheet-like objects, hence the name "worksheet"',
-  version: '0.0.4',
-  git: 'https://github.com/chicagogrooves/deanius-worksheet'
+  summary: "Turn JS objects into reactive Spreadsheet-like objects, hence the name "worksheet"",
+  version: "0.0.5",
+  git: "https://github.com/chicagogrooves/deanius-worksheet"
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('METEOR@1.0');
-  api.use('reactive-var');
-  api.addFiles('deanius:worksheet.js', 'client');
-  api.export('worksheet', ['client']);
+  api.versionsFrom("METEOR@1.0");
+  api.use(["tracker", "reactive-var"]);
+  api.addFiles("deanius:worksheet.js", "client");
+  api.export("worksheet", ["client"]);
 });
 
 Package.onTest(function(api) {
-  api.use('tinytest');
-  api.use('deanius:worksheet');
-  api.addFiles('deanius:worksheet-tests.js');
+  api.use("tinytest");
+  api.use("deanius:worksheet");
+  api.addFiles("deanius:worksheet-tests.js");
 });
